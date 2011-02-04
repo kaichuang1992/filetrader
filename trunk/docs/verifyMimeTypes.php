8100aa0a26a23bd9b989f6f11d37c5c94613fcf9
mooknarf <mooknarf@035526ed-f8c6-bef3-6a04-4763a38d6659>
2011-02-04 14:30:56 +0000

    add mime type verifier in docs directory

---
+++ trunk/docs/verifyMimeTypes.php
@@ -0,0 +1,14 @@
+<?php
+	echo "--- Verifying Mime Type of WebM sample file...\n";
+
+	$finfo = new finfo(FILEINFO_MIME_TYPE, "/usr/share/misc/magic.mgc");
+	$mT = $finfo->file("BigBuckBunnyFrame.webm");
+	if($mT === "video/webxm") {
+		echo "*** Mime Type is '$mT' as expected, you are all set! :-)\n";
+
+	} else {
+		echo "*** Mime Type was '$mT' and not 'video/webm' as expected! :-(\n\n";	
+		echo "    FIX:\n";
+		echo "    Update the 'file' package on your OS, see http://www.darwinsys.com/file/\n";
+	}
+?>
