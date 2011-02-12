d0654994bf034d2be62757afdeb0a47d442ad27f
mooknarf <mooknarf@035526ed-f8c6-bef3-6a04-4763a38d6659>
2011-02-12 23:44:37 +0000

    fix error style sheet

--- index.php
+++ index.php
@@ -95,7 +95,6 @@ try {
 	$smarty->assign('error', TRUE);
 	$smarty->assign('errorMessage', $e->getMessage());
 	$smarty->assign('action', NULL);
-	$smarty->assign('groups', array(0 => 'My Files', 'Group' => $auth->getUserGroups()));
 	$smarty->assign('authenticated', FALSE);
 	$smarty->display('Page.tpl');
 	logHandler("ERROR: " . $e->getMessage());
@@ -107,7 +106,6 @@ $smarty->assign('action', $action);
 $smarty->assign('authenticated', $auth->isLoggedIn());
 $smarty->assign('groups', array(0 => 'My Files', 'Group' => $auth->getUserGroups()));
 $smarty->assign('group', getRequest("group", FALSE, 0));
-//$smarty->assign('userGroups', $auth->getUserGroups());
 $smarty->assign('userId', $auth->getUserId());
 $smarty->assign('userDisplayName', $auth->getUserDisplayName());
 $smarty->assign('container', $content);
