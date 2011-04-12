684956d15dc74e51c17cf67b54a3ff08e231bd56
mooknarf <mooknarf@035526ed-f8c6-bef3-6a04-4763a38d6659>
2011-04-12 19:26:48 +0000

    update fancy upload

--- docs/blobUploadFancy.php
+++ docs/blobUploadFancy.php
@@ -24,16 +24,54 @@ if(isset($_REQUEST['action']) && $_REQUEST['action'] == 'upload') {
 <head>
 <meta charset="utf-8">
 <title>File Upload</title>
+<style type="text/css">
+	table { 
+		width: 100%;
+		border: 1px solid #000; 
+	}
+	
+	tfoot td {
+		border-top: 1px dashed #000;
+		padding-top: 5px;
+	}
+
+	th { 
+		text-align: left;
+	}
+	
+	td.noFiles {
+		text-align: center;
+		font-size: 85%;
+		font-style: italic;
+	}
+	span.done {
+		color: green;
+	}
+	span.cancelled {
+		color: blue;
+	}
+</style>
 </head>
 <body>
 <h1>File Upload</h1>
-<p>Select one or more files to upload...</p>
-<input id="inputFiles" type="file" onchange="handleFiles(this.files)" multiple>
-<button id="startButton" onclick="startUpload()" disabled>Start Upload</button>
-<button id="stopButton" onclick="stopUpload()" disabled>Stop Upload</button>
-<ul id="progress">
-</ul>
-<span id="result"></span>
+<table>
+	<thead>
+		<tr><th>File Name</th><th>File Size</th><th>Progress</th></tr>
+	</thead>
+	<tbody id="fileList">
+		<tr><td class="noFiles" colspan="3">No files selected yet...</td></tr>
+	</tbody>
+	<tfoot>
+		<tr><td colspan="2">
+			<input id="inputFiles" type="file" onchange="handleFiles(this.files)" multiple>
+			<button id="startButton" onclick="startUpload()" disabled>Start Upload</button>
+			<button id="stopButton" onclick="stopUpload()" disabled>Stop Upload</button>
+		</td>
+		<td><span id="result"></span></td>
+		</tr>
+	</tfoot>
+</table>
+
 <script>
 	var files; /* keep track of the files to upload */
 	var xhrs; /* keep track of all xhrs to be able to stop them */
@@ -43,12 +81,13 @@ if(isset($_REQUEST['action']) && $_REQUEST['action'] == 'upload') {
 	/* add the files to the upload list */
         function handleFiles(f) {
 		files = f;
-                var progress = document.getElementById('progress');
-		progress.innerHTML = '';
+                var fileList = document.getElementById('fileList');
+		document.getElementById('result').textContent = '';
+		fileList.innerHTML = '';
 		for (var i = 0; i < files.length; i++) {
-			var li = document.createElement('li');
-			li.innerHTML = files[i].name + ' (<strong><span style="color: blue" id="file_progress_' + i + '">0%</span></strong>)';
-			progress.appendChild(li);
+			var tr = document.createElement('tr');
+			tr.innerHTML = '<td>' + files[i].name + '</td><td>' + files[i].size + '</td><td><span id="file_progress_' + i + '"></span></td>';
+			fileList.appendChild(tr);
 		}
 		if(files.length != 0) {
 			document.getElementById('startButton').removeAttribute('disabled');
@@ -74,6 +113,9 @@ if(isset($_REQUEST['action']) && $_REQUEST['action'] == 'upload') {
 			xhrs[i].abort();
 		}
                 document.getElementById('stopButton').setAttribute('disabled','disabled');
+                document.getElementById('result').textContent = "Canceled";
+                document.getElementById('result').setAttribute('class', 'cancelled');
+
 	}
 
         function upload(index,file) {
@@ -90,16 +132,13 @@ if(isset($_REQUEST['action']) && $_REQUEST['action'] == 'upload') {
 
 		/* when upload is complete */
 		xhr.upload.addEventListener("load", function(evt) {
-			document.getElementById('file_progress_'+index).innerHTML = '<span style="color: green">Done</span>';
+			document.getElementById('file_progress_'+index).textContent = "Done";
+			document.getElementById('file_progress_'+index).setAttribute('class', 'done');
 			/* if this was the last file, disable stop button */
 			if(++done == files.length) {
 		                document.getElementById('stopButton').setAttribute('disabled','disabled');
-				var endTime = new Date().getTime();
-				var elapsedTime = (endTime - startTime) / 1000;
-				var totalSize = 0;
-				for(var i=0;i<files.length;i++)
-					totalSize+=files[i].size;
-				document.getElementById('result').textContent = "All done! Transfered " + totalSize + " bytes in " + elapsedTime + " seconds (" + Math.round((totalSize / elapsedTime) / 1024) + "kB/s)";
+				document.getElementById('result').textContent = "Done";
+	                        document.getElementById('result').setAttribute('class', 'done');
 			}
 		}, false);
 
