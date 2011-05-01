f5bc147445e59effff2780776b4023457e876e52
mooknarf <mooknarf@035526ed-f8c6-bef3-6a04-4763a38d6659>
2011-05-01 20:16:12 +0000

    fileapi spec breakage, dealing better with slice, mozSlice, webkitSlice

--- trunk/docs/blobUploadFancySlices.php
+++ trunk/docs/blobUploadFancySlices.php
@@ -158,7 +158,12 @@ if(array_key_exists('X-Requested-With', $httpHeaders) && $httpHeaders['X-Request
 					if(bytesLeft > 0) {
 						transferLength = (blockSize > bytesLeft) ? bytesLeft : blockSize;
 						currentChunk++;
-						blob = file.slice(currentChunk*blockSize, transferLength);
+						if(file.slice)
+							blob = file.slice(currentChunk*blockSize, transferLength);
+						if(file.mozSlice)
+	                                                blob = file.mozSlice(currentChunk*blockSize, currentChunk*blockSize+transferLength);
+						if(file.webkitSlice)
+		                                        blob = file.webkitSlice(currentChunk*blockSize, currentChunk*blockSize+transferLength);
 						reader.readAsBinaryString(blob);
 					} else {
 						/* all done */
@@ -180,7 +185,12 @@ if(array_key_exists('X-Requested-With', $httpHeaders) && $httpHeaders['X-Request
 				xhr.setRequestHeader("X-File-Chunk", currentChunk);
 				xhr.send(blob);
 			}
-			blob = file.slice(0, transferLength);
+                        if(file.slice)
+                                blob = file.slice(0, transferLength);
+                        if(file.mozSlice)
+                                blob = file.mozSlice(0, currentChunk*blockSize+transferLength);
+                        if(file.webkitSlice)
+                              blob = file.webkitSlice(0, currentChunk*blockSize+transferLength);
 			reader.readAsBinaryString(blob);
 		}
         }
