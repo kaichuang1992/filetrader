60e773012e0eec7fefdd778354bda304210b9381
mooknarf <mooknarf@035526ed-f8c6-bef3-6a04-4763a38d6659>
2011-06-02 18:57:01 +0000

    initial attempt at uploading

--- ftc/j/handler.js
+++ ftc/j/handler.js
@@ -1,53 +1,71 @@
-$(document).ready(function () {
-    $("button").click(function (event) {
-        var actionType = $(this).attr('id');
-        $.post('?action=' + actionType, {
-            dirName: $("#dirName").val()
-        });
-        event.preventDefault();
+$(document).ready(
+    function() {
+	    $("button").click(function(event) {
+		    var actionType = $(this).attr('id');
+		    $.post('?action=' + actionType, {
+			    dirName : $("#dirName").val() });
+		    event.preventDefault();
+	    });
+
+	    $("a.file").live('click', function(event) {
+		    var fileName = $(this).text();
+		    $.post('?action=getDownloadToken', {
+			    fileName : fileName }, function(data) {
+			    var resp = jQuery.parseJSON(data);
+			    window.location.href = resp.downloadLocation;
+		    });
+		    event.preventDefault();
+	    });
+
+	    $("a.menu").live(
+	        'click',
+	        function(event) {
+		        var actionType = $(this).attr('id');
+
+		        $.getJSON('?action=' + actionType, function(data) {
+			        var items = [];
+
+			        if (actionType === 'getFileList') {
+				        $.each(data.files, function(key, val) {
+					        if (val.isDirectory) {
+						        items.push('<tr><th>' + key + '</th><td>[DIR]</td></tr>>');
+					        } else {
+						        items.push('<tr><th><a href="#" class="file">' + key
+						            + '</a></th><td>' + val.fileSize + '</td></tr>>');
+					        }
+				        });
+			        } else {
+				        $.each(data, function(key, val) {
+					        items.push('<tr><th>' + key + '</th><td>' + val
+					            + '</td></tr>>');
+				        });
+			        }
+			        $("#output").html($('<table/>', {
+			          'class' : 'my-new-list',
+			          html : items.join('') }));
+
+		        });
+		        event.preventDefault();
+	        });
+
+	    $("input").change(function(event) {
+		    var f = this.files[0];
+
+		    var fileName = $(this).text();
+		    $.post('?action=getUploadToken', {
+		      fileName : f.name,
+		      fileSize : f.size }, function(data) {
+			    var resp = jQuery.parseJSON(data);
+			    var uploadUrl = resp.uploadLocation;
+			    alert(uploadUrl);
+		    });
+		    event.preventDefault();
+
+		    // var xhr = new XMLHttpRequest();
+		    // xhr.open("PUT", 'http://192.87.110.161<?php echo
+		    // $_SERVER['SCRIPT_NAME']; ?>?action=upload&fileName='+file.name,
+		    // true);
+		    // xhr.setRequestHeader('X-File-Name', file.name);
+		    // xhr.send(file);
+	    });
     });
-
-    $("a.file").live('click', function (event) {
-        var fileName = $(this).text();
-        $.post('?action=getDownloadToken', {
-            fileName: fileName
-        }, function (data) {
-            var resp = jQuery.parseJSON(data);
-            window.location.href = resp.downloadLocation;
-        });
-        event.preventDefault();
-    });
-
-    $("a.menu").live('click', function (event) {
-        var actionType = $(this).attr('id');
-
-        $.getJSON('?action=' + actionType, function (data) {
-            var items = [];
-
-            if (actionType === 'getFileList') {
-                $.each(
-                data.files, function (
-                key, val) {
-                    if (val.isDirectory) {
-                        items.push('<tr><th>' + key + '</th><td>[DIR]</td></tr>>');
-                    } else {
-                        items.push('<tr><th><a href="#" class="file">' + key + '</a></th><td>' + val.fileSize + '</td></tr>>');
-                    }
-                });
-            } else {
-                $.each(
-                data, function (
-                key, val) {
-                    items.push('<tr><th>' + key + '</th><td>' + val + '</td></tr>>');
-                });
-            }
-            $("#output").html(
-            $('<table/>', {
-                'class': 'my-new-list',
-                html: items.join('')
-            }));
-
-        });
-        event.preventDefault();
-    });
-});
