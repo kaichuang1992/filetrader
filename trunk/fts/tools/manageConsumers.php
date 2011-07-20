62b9d90a0ff9ef8afa13f8b2380f8cad0eed6ed0
mooknarf@gmail.com <mooknarf@gmail.com@035526ed-f8c6-bef3-6a04-4763a38d6659>
2011-07-20 10:49:13 +0000

    - update manageConsumers tool to support RSA public certificates as well - add support for oauth.net PHP consumer library because it supports RSA - TODO: still need to switch to this other OAuth library, deal with Fedora   packaging first...

--- trunk/fts/tools/manageConsumers.php
+++ trunk/fts/tools/manageConsumers.php
@@ -5,12 +5,13 @@
 	$commands = array('list','add','del');
 
 	if($argc < 2) {
-		echo "specify a command [" . implode(", ", $commands) . "]\n";
+		showHelp();
 		exit(1);
 	}
 
 	if(!in_array($argv[1], $commands)) {
-		echo "invalid command specified [" . implode(", ", $commands) . "]\n";
+		echo "Invalid command!\n";
+		showHelp();
 		exit(1);
 	}
 
@@ -43,14 +44,19 @@
 		$stmt->bindValue(':key', $argv[2]);
 		if($argc > 3) {
 			/* secret specified */
-                	$secret = $argv[3];
+			if(file_exists($argv[3]) && is_file($argv[3])) {
+				/* certificate instead of secret specified */
+				$secret = file_get_contents($argv[3]);
+			} else {
+	                	$secret = $argv[3];
+			}
 		} else {
 			/* generate us a new secret */
 			$secret = generateToken();
 		}
 		$stmt->bindValue(':secret', $secret);
 		$stmt->execute();
-		echo "consumerKey: [" . $argv[2] . "], consumerSecret: [" . $secret . "]\n";
+		echo "ADDED consumer '" . $argv[2] . "'\n";
 		break;
 
 	case "del":
@@ -62,7 +68,26 @@
                 $stmt = $dbh->prepare('DELETE FROM storageConsumers WHERE consumerKey = :key');
                 $stmt->bindValue(':key', $argv[2]);
                 $stmt->execute();
-                echo "storage consumer [" . $argv[2] . "] deleted\n";
+                echo "DELETED consumer '" . $argv[2] . "'\n";
                 break;
+	}
+
+	function showHelp() {
+		echo "Manage the OAuth consumers\n\n";
+		echo "Add with specified secret        add <consumer_key> <consumer_secret>\n";
+		echo "Add with specified certificate   add <consumer_key> <consumer_certificate>\n";
+		echo "Add with generated secret        add <consumer_key>\n";
+		echo "\n";
+		echo "Delete consumer                  del <consumer_key>\n";
+		echo "\n";
+		echo "List consumers and secrets       list\n";
+		echo "\n";
+		echo "Examples:\n";
+		echo "\n";
+		echo "add www.google.com igoogle.pem\n";
+		echo "\twhere 'igoogle.pem' is the file containing the iGoogle certificate\n";
+		echo "add test_key test_secret\n";
+		echo "add another_test_key\n";
+		echo "\n";	
 	}
 ?> 
