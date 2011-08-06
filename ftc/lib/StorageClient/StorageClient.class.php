9c7567a6a41a1985dda6d599aa53ad4297505f5f
mooknarf <mooknarf@gmail.com>
2011-08-06 22:14:50 +0000

    fix path to OAuth library

--- ftc/lib/StorageClient/StorageClient.class.php
+++ ftc/lib/StorageClient/StorageClient.class.php
@@ -18,7 +18,7 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-require_once("../fts/ext/oauth/OAuth.php");
+require_once("OAuth.php");
 
 class StorageClient {
     private $consumer;
