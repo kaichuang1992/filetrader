1412ebd25c6828d86a4cc3a65d503638ac8098ce
mooknarf <mooknarf@gmail.com>
2011-08-06 22:10:48 +0000

    new ftc!

---
+++ ftc/j/ftc.js
@@ -0,0 +1,125 @@
+$(document).ready(function () {
+    var proxy_endpoint = "proxy.php";
+    var params = {
+        proxy_to: "http://192.87.110.161/fts/index.php",
+        proxy_consumer_key: "demo",
+        proxy_consumer_secret: "a1bf8348016c52f81498cd576d55a932",
+        relativePath: "/",
+    };
+
+    function getDirList() {
+        params.action = 'getDirList';
+        $.get(proxy_endpoint, params, function (response) {
+            $.each(response.data, function (i, v) {
+                v.fileDate = fancyDate(v.fileDate);
+                v.fileSize = toHumanSize(v.fileSize);
+            });
+
+            // this is ugly do not want!
+            $("tbody").html($("#directoryEntryHeader").tmpl());
+            $("#directoryEntry").tmpl(response.data).appendTo("tbody");
+            $('tr:odd').each(function () {
+                // TODO: set style class instead of CSS right here
+                $(this).css('background-color', '#ccc');
+            });
+            $('a.download').click(function (event) {
+                var tmp_rp = params.relativePath;
+                params.action = 'getDownloadToken';
+                params.relativePath += "/" + $(this).parent().next().text();
+                $.post(proxy_endpoint, params, function (response) {
+                    window.location.href = response.data.downloadLocation;
+                }, 'json');
+                params.relativePath = tmp_rp;
+                getDirList();
+            });
+            $('a.open').click(function (event) {
+                params.relativePath += "/" + $(this).parent().next().text();
+                getDirList();
+            });
+            $('a.delete').click(function (event) {
+                var fileName = $(this).parent().prev().prev().prev().text();
+                if (confirm("Are you sure you want to delete '" + fileName + "'?")) {
+                    var tmp_rp = params.relativePath;
+                    params.action = 'deleteFile';
+                    params.relativePath += "/" + fileName;
+                    $.post(proxy_endpoint, params, function (response) {
+                        params.relativePath = tmp_rp;
+                        getDirList();
+                    }, 'json');
+                }
+            });
+        }, 'json');
+    }
+
+    function fancyDate(ts) {
+        var currentTs = new Date().getTime();
+        var date = new Date(ts * 1000);
+        var humanDate;
+        if (ts * 1000 > (currentTs - 60 * 60 * 24 * 1000)) {
+            // last 24h, show just time
+            var hours = (date.getHours() < 10) ? "0" + date.getHours() : date.getHours();
+            var minutes = (date.getMinutes() < 10) ? "0" + date.getMinutes() : date.getMinutes();
+            humanDate = hours + ":" + minutes;
+        } else if (ts * 1000 > (currentTs - 60 * 60 * 24 * 7 * 1000)) {
+            // last week, show just day and time
+            var hours = (date.getHours() < 10) ? "0" + date.getHours() : date.getHours();
+            var minutes = (date.getMinutes() < 10) ? "0" + date.getMinutes() : date.getMinutes();
+            humanDate = dayToText(date.getDay()) + " " + hours + ":" + minutes;
+        } else if (ts * 1000 > (currentTs - 60 * 60 * 24 * 365 * 1000)) {
+            // longer ago, show just month+day
+            humanDate = monthToText(date.getMonth()) + " " + date.getDate();
+        } else {
+            // show year 
+            humanDate = date.getFullYear();
+        }
+        return humanDate;
+    }
+
+    function dayToText(day) {
+        var days = {
+            0: "Sun",
+            1: "Mon",
+            2: "Tue",
+            3: "Wed",
+            4: "Thu",
+            5: "Fri",
+            6: "Sat"
+        };
+        return days[day];
+    }
+
+    function monthToText(month) {
+        var months = {
+            0: "Jan",
+            1: "Feb",
+            2: "Mar",
+            3: "Apr",
+            4: "May",
+            5: "Jun",
+            6: "Jul",
+            7: "Aug",
+            8: "Sep",
+            9: "Oct",
+            10: "Nov",
+            11: "Dec"
+        };
+        return months[month];
+    }
+
+    function toHumanSize(bytes) {
+        var kilobyte = 1024;
+        var megabyte = kilobyte * kilobyte;
+        var gigabyte = megabyte * kilobyte;
+        if (bytes >= gigabyte) {
+            return Math.round((bytes / gigabyte)) + "GB"
+        };
+        if (bytes >= megabyte) {
+            return Math.round((bytes / megabyte)) + "MB"
+        };
+        if (bytes >= kilobyte) {
+            return Math.round((bytes / kilobyte)) + "kB"
+        };
+        return bytes;
+    }
+    getDirList();
+});
---
+++ ftc/proxy.php
@@ -0,0 +1,60 @@
+<?php
+
+/*
+ *  FileTrader - Web based file sharing platform
+ *  Copyright (C) 2011 François Kooman <fkooman@tuxed.net>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+require_once('config.php');
+require_once('utils.php');
+
+require_once('lib/StorageClient/StorageClient.class.php');
+
+/* Disable Caching */
+header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
+
+if (!isset($config) || !is_array($config)) {
+    die("broken or missing configuration file?");
+}
+
+date_default_timezone_set(getConfig($config, 'time_zone', FALSE, 'Europe/Amsterdam'));
+
+try {
+    if (getConfig($config, 'ssl_only', FALSE, FALSE)) {
+        if (getProtocol() != "https://") {
+            throw new Exception("only available through secure connection");
+        }
+    }
+
+    set_include_path(get_include_path() . PATH_SEPARATOR . getConfig($config, "oauth_lib_dir", TRUE));
+
+	$params = $_REQUEST;
+	$endpoint = $params['proxy_to'];
+	$key = $params['proxy_consumer_key'];
+	$secret = $params['proxy_consumer_secret'];
+	$action = $params['action'];
+	unset($params['proxy_to']);
+	unset($params['proxy_consumer_key']);
+	unset($params['proxy_consumer_secret']);
+	unset($params['action']);
+
+        $sc = new StorageClient(array('apiUrl' => $endpoint, 'consumerKey' => $key, 'consumerSecret' => $secret ));
+        echo $sc->call($action, $params, $_SERVER['REQUEST_METHOD']);
+        exit(0);
+} catch (Exception $e) {
+}
+?>
